92696e442eb6ab2fa6b4dcda8c8cd3c3632da41c
ESAditya1729 <adityamukherjee09949@gmail.com>
2026-03-04 01:15:35 +0530

    changed backend url in shop page

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -68,7 +68,7 @@ const Products = () => {
   const [userRole, setUserRole] = useState("user");
   const [categoriesLoaded, setCategoriesLoaded] = useState(false);
 
-  const API_URL = "http://localhost:5000/api"; //process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  const API_URL =  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
   const PRODUCTS_PER_PAGE = 12;
 
   // Sample tags for filtering
